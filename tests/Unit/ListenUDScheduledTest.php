8f74e9bf21ecf61453777972698af282132123ed
Futur <futur@futurfuturfutur.com>
2020-12-11 17:50:59 +0200

    Add not due expression test

--- tests/Unit/ListenUDScheduledTest.php
+++ tests/Unit/ListenUDScheduledTest.php
@@ -29,4 +29,22 @@ class ListenUDScheduledTest extends TestCase
             $result === 'Successfully processed UDScheduled task for scheduler with email - ' . $scheduler->email
         );
     }
+
+    public function testListenUDScheduledTestWithNotDueExpression()
+    {
+        $scheduler = Scheduler::first();
+        $scheduler->expressions()->save(new Expression([
+            'expression' => '59 23 4 * 2',
+            'schedulable' => UDScheduledObject::class
+        ]));
+
+        ob_start();
+        UDSchedule::listenExpressions();
+        $result = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertTrue(
+            $result !== 'Successfully processed UDScheduled task for scheduler with email - ' . $scheduler->email
+        );
+    }
 }
