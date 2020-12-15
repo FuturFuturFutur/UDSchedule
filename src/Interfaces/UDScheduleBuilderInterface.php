52a312f9a685d836bef1fe20dddab1e55cc509f2
Futur <futur@futurfuturfutur.com>
2020-12-15 14:54:10 +0200

    Add expressions generator methods

--- src/Interfaces/UDScheduleBuilderInterface.php
+++ src/Interfaces/UDScheduleBuilderInterface.php
@@ -10,7 +10,21 @@ interface UDScheduleBuilderInterface
 
     public function withSchedulable(string $schedulable) : UDScheduleBuilderInterface;
 
-    public function monthly(string $value);
+    public function monthly($dayOfMonth);
 
-    public function weekly(string $value);
+    public function lastDayOfMonth();
+
+    public function weekly($dayOfWeek);
+
+    public function weekdays();
+
+    public function weekends();
+
+    public function daily($time);
+
+    public function at($time);
+
+    public function custom($expression);
+
+    public function timezone($timezone);
 }
